6e3149a09d923383d07573ca3ef993c08e3f79b6
Erik5CA <erik.castillo.anzures@gmail.com>
2024-04-16 17:56:36 -0600

    mocks

---
+++ src/mocks/linkObj.js
@@ -0,0 +1,31 @@
+import linkedin from "../assets/linkedin.svg";
+import github from "../assets/github.svg";
+import code from "../assets/code.svg";
+import demo from "../assets/demo.svg";
+
+export const objs = [
+  {
+    name: "LinkedIn",
+    url: "https://github.com/ErikCastillo",
+    icon: linkedin,
+    class: "linkedin",
+  },
+  {
+    name: "Github",
+    url: "https://github.com/ErikCastillo",
+    icon: github,
+    class: "github",
+  },
+  {
+    name: "Code",
+    url: "https://github.com/ErikCastillo",
+    icon: code,
+    class: "code",
+  },
+  {
+    name: "Demo",
+    url: "https://github.com/ErikCastillo",
+    icon: demo,
+    class: "demo",
+  },
+];
---
+++ src/mocks/listTech.js
@@ -0,0 +1,59 @@
+import html from "../assets/tec/html5.svg";
+import css from "../assets/tec/css3.svg";
+import django from "../assets/tec/django.svg";
+import javascript from "../assets/tec/javascript.svg";
+import python from "../assets/tec/python.svg";
+import react from "../assets/tec/react.svg";
+import git from "../assets/tec/git.svg";
+
+export const techs = [
+  {
+    id: 1,
+    name: "HTML5",
+    class: "html",
+    image: html,
+    url: "https://www.w3schools.com/html/",
+  },
+  {
+    id: 2,
+    name: "CSS3",
+    class: "css",
+    image: css,
+    url: "https://www.w3schools.com/css/",
+  },
+  {
+    id: 3,
+    name: "Django",
+    class: "django",
+    image: django,
+    url: "https://www.djangoproject.com/",
+  },
+  {
+    id: 4,
+    name: "Javascript",
+    class: "javascript",
+    image: javascript,
+    url: "https://www.w3schools.com/js/",
+  },
+  {
+    id: 5,
+    name: "Python",
+    class: "python",
+    image: python,
+    url: "https://www.w3schools.com/python/",
+  },
+  {
+    id: 6,
+    name: "React",
+    class: "react",
+    image: react,
+    url: "https://www.w3schools.com/python/",
+  },
+  {
+    id: 7,
+    name: "Git",
+    class: "git",
+    image: git,
+    url: "https://www.w3schools.com/python/",
+  },
+];
