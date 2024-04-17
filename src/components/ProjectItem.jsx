6968471814b69af6475571f2a76a2fca87a503aa
Erik5CA <erik.castillo.anzures@gmail.com>
2024-04-17 17:42:12 -0600

    show correct info for projects and added techs

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,21 +1,25 @@
 import { ButtonLink } from "./ButtonLink";
 import { objs } from "../mocks/linkObj";
-export function ProjectItem({ image }) {
+export function ProjectItem({ project }) {
   return (
     <div className="grid-projects-item">
       <div className="grid-projects-item-image">
-        <img src={image} alt="" />
+        <img src={project.image} alt="" />
       </div>
       <div className="grid-projects-item-content">
-        <h3>Project 1</h3>
+        <h3 className="title-project">{project.title}</h3>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-          quasi. Qui, quaerat.
+          {project.description}
         </p>
         <div className="container-project-links">
           <ButtonLink obj={objs[2]} />
           <ButtonLink obj={objs[3]} />
         </div>
+        <dir className="container-project-techs">
+          {project.techs.map((tech) => (
+            <h4 key={tech.id}>{tech}</h4>
+          ))}
+        </dir>
       </div>
     </div>
   );
