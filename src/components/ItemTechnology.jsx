b9d359ee724b0d46ec18cd97e560c866f4a041c0
Erik5CA <erik.castillo.anzures@gmail.com>
2024-04-17 17:39:20 -0600

    delete container for each tech

--- src/components/ItemTechnology.jsx
+++ src/components/ItemTechnology.jsx
@@ -3,10 +3,10 @@ export function ItemTechnology({tech}) {
 
   return (
     <div className="container-technology">
-      <div className={`${tech.class}`}>
+      {/* <div className={`${tech.class}`}> */}
         <img src={tech.image} alt="" />
         <p>{tech.name}</p>
-      </div>
+      {/* </div> */}
     </div>
   );
 }
