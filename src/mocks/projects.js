5812dba946ad875af896723d22c4acc230db1165
Erik5CA <erik.castillo.anzures@gmail.com>
2024-04-17 17:43:27 -0600

    list of objects with the info of the projects

---
+++ src/mocks/projects.js
@@ -0,0 +1,38 @@
+// List a array the projects, with title, description, image, url and list of tecnologies
+import p1 from "../public/p1.jpg";
+const projects = [
+  {
+    id: 1,
+    title: "Project 1",
+    description: "Project 1 description",
+    image: p1,
+    url: "https://github.com/ErikCastillo/portfolio",
+    techs: ["Javascript", "Html", "CSS", "React"],
+  },
+  {
+    id: 2,
+    title: "Project 2",
+    description: "Project 2 description",
+    image: p1,
+    url: "https://github.com/ErikCastillo/portfolio",
+    techs: ["Javascript", "Html", "CSS", "React"],
+  },
+  {
+    id: 3,
+    title: "Project 3",
+    description: "Project 3 description",
+    image: p1,
+    url: "https://github.com/ErikCastillo/portfolio",
+    techs: ["Javascript", "Html", "CSS", "React"],
+  },
+  {
+    id: 4,
+    title: "Project 4",
+    description: "Project 4 description",
+    image: p1,
+    url: "https://github.com/ErikCastillo/portfolio",
+    techs: ["Javascript", "Html", "CSS", "React"],
+  },
+];
+
+export default projects;
