fa92cbec44dc9ba7e0b86edfd5b3e632cf2594da
Erik5CA <erik.castillo.anzures@gmail.com>
2024-04-17 17:38:15 -0600

    map all projects

--- src/components/GridProjects.jsx
+++ src/components/GridProjects.jsx
@@ -2,16 +2,16 @@ import { ProjectItem } from "./ProjectItem";
 import "../styles/GridProjects.css";
 import p1 from '../public/p1.jpg'
 import p2 from '../public/p2.jpg'
+import projects from '../mocks/projects'
 export function GridProjects() {
   return (
-    <div className="container-projects">
+    <div className="container-projects" id="projects">
       <h2>Projects</h2>
       <div className="grid-projects">
         
-        <ProjectItem image={p1} />
-        <ProjectItem image={p1} />
-        <ProjectItem image={p1} />
-        <ProjectItem image={p2} />
+        {projects.map((project) => (
+          <ProjectItem key={project.id} project={project} />
+        ))}
 
       </div>
     </div>
