344ef01df3c739ecf61a426f90808e170476dea2
Erik5CA <erik.castillo.anzures@gmail.com>
2024-04-18 16:58:25 -0600

    updated title and removed images

--- src/components/GridProjects.jsx
+++ src/components/GridProjects.jsx
@@ -1,12 +1,10 @@
 import { ProjectItem } from "./ProjectItem";
 import "../styles/GridProjects.css";
-import p1 from '../public/p1.jpg'
-import p2 from '../public/p2.jpg'
 import projects from '../mocks/projects'
 export function GridProjects() {
   return (
     <div className="container-projects" id="projects">
-      <h2>Projects</h2>
+      <h2>Proyectos</h2>
       <div className="grid-projects">
         
         {projects.map((project) => (
