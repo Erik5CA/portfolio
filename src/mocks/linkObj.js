a241cdbcbad9fb6ff2caa0b4feb32ed99899c42d
Erik5CA <erik.castillo.anzures@gmail.com>
2024-04-18 17:01:32 -0600

    updated urls and added new object for cv

--- src/mocks/linkObj.js
+++ src/mocks/linkObj.js
@@ -2,29 +2,37 @@ import linkedin from "../assets/linkedin.svg";
 import github from "../assets/github.svg";
 import code from "../assets/code.svg";
 import demo from "../assets/demo.svg";
+import file from "../assets/file.svg";
+import cv from "../../public/ErikCastilloCV.pdf";
 
 export const objs = [
   {
     name: "LinkedIn",
-    url: "https://github.com/ErikCastillo",
+    url: "https://www.linkedin.com/in/erik-castillo-b48b12239/",
     icon: linkedin,
     class: "linkedin",
   },
   {
     name: "Github",
-    url: "https://github.com/ErikCastillo",
+    url: "https://github.com/Erik5CA",
     icon: github,
     class: "github",
   },
   {
+    name: "Curriculum",
+    url: cv,
+    icon: file,
+    class: "cv",
+  },
+  {
     name: "Code",
-    url: "https://github.com/ErikCastillo",
+    url: false,
     icon: code,
     class: "code",
   },
   {
     name: "Demo",
-    url: "https://github.com/ErikCastillo",
+    url: false,
     icon: demo,
     class: "demo",
   },
