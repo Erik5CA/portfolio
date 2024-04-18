321b9ce9472171f809f17b6ed501f297242ec5da
Erik5CA <erik.castillo.anzures@gmail.com>
2024-04-18 16:59:45 -0600

    updated tiltes and added url for download cv

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import "../styles/Navbar.css";
 import list from '../assets/list.svg'
 import close from "../assets/close.svg";
+import cv from '../../public/ErikCastilloCV.pdf'
 import { useState } from "react";
 
 export function Navbar() {
@@ -17,39 +18,45 @@ export function Navbar() {
       <div>
         <ul className="container-navbar-links">
           <li>
-            <a href="#about">About</a>
+            <a href="#about">Sobre mí</a>
           </li>
           <li>
-            <a href="#projects">Projects</a>
+            <a href="#projects">Proyectos</a>
           </li>
           <li>
-            <a href="#contact">Contact</a>
+            <a href="#contact">Contacto</a>
           </li>
           <li>
-            <a href="#resume">Resume</a>
+            <a href={cv} target="_blank" rel="noreferrer">
+              CV
+            </a>
           </li>
         </ul>
       </div>
       <button className="burger-button" onClick={handleChange}>
         <img src={show ? close : list} alt="" />
       </button>
-      <div className={`menu-mobile ${show ? 'show' : ''}`}
-      onClick={handleChange}>
+      <div
+        className={`menu-mobile ${show ? "show" : ""}`}
+        onClick={handleChange}
+      >
         <ul className="container-navbar-links-mobile">
           <button className="button-close" onClick={handleChange}>
             <img src={close} alt="" />
           </button>
           <li>
-            <a href="#about">About</a>
+            <a href="#about">Sobre mí</a>
           </li>
           <li>
-            <a href="#projects">Projects</a>
+            <a href="#projects">Proyectos</a>
           </li>
           <li>
-            <a href="#contact">Contact</a>
+            <a href="#contact">Contacto</a>
           </li>
           <li>
-            <a href="#resume">Resume</a>
+            <a href={cv} target="_blank" rel="noreferrer">
+              CV
+            </a>
           </li>
         </ul>
       </div>
