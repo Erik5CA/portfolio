abe9fe4c83de6e1b74943c0c7ef2309981bb8890
Erik5CA <erik.castillo.anzures@gmail.com>
2024-04-18 17:00:33 -0600

    added correct key for each tech

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,5 +1,7 @@
 import { ButtonLink } from "./ButtonLink";
 import { objs } from "../mocks/linkObj";
+
+// TODO: add corrects url for project demo
 export function ProjectItem({ project }) {
   return (
     <div className="grid-projects-item">
@@ -8,16 +10,14 @@ export function ProjectItem({ project }) {
       </div>
       <div className="grid-projects-item-content">
         <h3 className="title-project">{project.title}</h3>
-        <p>
-          {project.description}
-        </p>
+        <p>{project.description}</p>
         <div className="container-project-links">
-          <ButtonLink obj={objs[2]} />
-          <ButtonLink obj={objs[3]} />
+          <ButtonLink obj={objs[3]} url={project.url} />
+          <ButtonLink obj={objs[4]} url={project.url} />
         </div>
         <dir className="container-project-techs">
-          {project.techs.map((tech) => (
-            <h4 key={tech.id}>{tech}</h4>
+          {project.techs.map((tech, index) => (
+            <h4 key={index}>{tech}</h4>
           ))}
         </dir>
       </div>
