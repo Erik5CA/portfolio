8b13737a59c7459deb351b7f626312ce95a66943
Erik5CA <erik.castillo.anzures@gmail.com>
2024-04-23 17:42:22 -0600

    added correct url for code and demo for projects

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,7 +1,6 @@
 import { ButtonLink } from "./ButtonLink";
 import { objs } from "../mocks/linkObj";
 
-// TODO: add corrects url for project demo
 export function ProjectItem({ project }) {
   return (
     <div className="grid-projects-item">
@@ -12,8 +11,8 @@ export function ProjectItem({ project }) {
         <h3 className="title-project">{project.title}</h3>
         <p>{project.description}</p>
         <div className="container-project-links">
-          <ButtonLink obj={objs[3]} url={project.url} />
-          <ButtonLink obj={objs[4]} url={project.url} />
+          <ButtonLink obj={objs[3]} url={project.urlCode} />
+          <ButtonLink obj={objs[4]} url={project.urlDemo} />
         </div>
         <dir className="container-project-techs">
           {project.techs.map((tech, index) => (
