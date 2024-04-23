abf09c58864b9f42456d62e5dd887ab8030cd51d
Erik5CA <erik.castillo.anzures@gmail.com>
2024-04-23 17:43:34 -0600

    changed import of images for projects

--- src/mocks/projects.js
+++ src/mocks/projects.js
@@ -1,9 +1,9 @@
 // List a array the projects, with title, description, image, url and list of tecnologies
 // import p1 from "../public/p1.jpg";
-import py1 from "../../public/proyecto1.jpeg";
-import py2 from "../../public/proyecto2.jpeg";
-import py3 from "../../public/proyecto3.jpeg";
-import py4 from "../../public/proyecto4.jpeg";
+import py1 from "../public/proyecto1.jpeg";
+import py2 from "../public/proyecto2.jpeg";
+import py3 from "../public/proyecto3.jpeg";
+import py4 from "../public/proyecto4.jpeg";
 const projects = [
   {
     id: 1,
@@ -11,7 +11,8 @@ const projects = [
     description:
       "App para crear grupos de estudio sobre cualquier tema, con la posibilidad de unirte para crear comentrios y ver activiades reciente, cuenta con autenticación de usuarios.",
     image: py1,
-    url: "https://github.com/Erik5CA/django-study-app",
+    urlCode: "https://github.com/Erik5CA/django-study-app",
+    urlDemo: "https://django-study-app.onrender.com",
     techs: ["Javascript", "Html", "CSS", "Django", "PostgreSQL"],
   },
   {
@@ -20,7 +21,8 @@ const projects = [
     description:
       "App para buscar artistas, ver su discografía y sus canciones, utilizando la API de Spotify.",
     image: py2,
-    url: "https://github.com/Erik5CA/Spotify-App",
+    urlCode: "https://github.com/Erik5CA/Spotify-App",
+    urlDemo: "https://erik5ca.github.io/Spotify-App/",
     techs: ["Javascript", "Html", "CSS", "React", "API Spotify"],
   },
   {
@@ -29,7 +31,9 @@ const projects = [
     description:
       "Web para buscar paises, ver informacion sobre ellos, ademas de poder elegir tema obscuro o claro. ",
     image: py3,
-    url: "https://github.com/Erik5CA/proyectos-frontend/tree/main/02-rest-countries",
+    urlCode: "https://github.com/Erik5CA/react-rest-countries",
+    urlDemo: "https://erik5ca.github.io/react-rest-countries/",
+
     techs: ["Javascript", "Html", "CSS", "React"],
   },
   {
@@ -38,7 +42,8 @@ const projects = [
     description:
       "Web para calcular las propinas deacuerdo a un monto, total de personas y el porcentaje.",
     image: py4,
-    url: "https://github.com/Erik5CA/proyectos-frontend/tree/main/01-tip-calculator",
+    urlCode: "https://github.com/Erik5CA/react-tip-calculator",
+    urlDemo: "https://erik5ca.github.io/react-tip-calculator/",
     techs: ["Javascript", "Html", "CSS", "React"],
   },
 ];
